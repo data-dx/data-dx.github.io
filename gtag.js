fb07fdd6065e26d232a5f43406bb08ed997069a4
data-dx <wst0329@gmail.com>
2025-12-25 13:25:42 +0800

    update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,8 +1,14 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DEKCKDRLSN"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-DEKCKDRLSN');
-</script>
+// js/gtag.js
+
+// Google tag (gtag.js)
+(function () {
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-DEKCKDRLSN';
+  document.head.appendChild(gtagScript);
+})();
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-DEKCKDRLSN');
